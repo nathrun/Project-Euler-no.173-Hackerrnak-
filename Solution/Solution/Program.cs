33250dd27050907e2e967248c229cc1d616940a6
Nathan Le Roux <nathrun007@gmail.com>
2017-06-07 20:22:37 +0200

    initial setup

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+        }
+    }
+}
