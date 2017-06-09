8e4d8d41a1d9872712c1a1395d58b9569e83846e
Nathan Le Roux <nathrun007@gmail.com>
2017-06-09 21:06:23 +0200

    First attempt at answer

--- Solution/Solution/Program.cs
+++ Solution/Solution/Program.cs
@@ -8,10 +8,39 @@ namespace Solution
 {
     class Solution
     {
+        static long Solve(int n)
+        {
+            long result = 0;
+            //code to find solution
+            while (n >= 4)
+            {
+                int x = (int)(n / 4);
+                if (n % 4 == 0)
+                {
+                    for (int k = 1; k*k < x; k++)
+                    {
+                        if (x % k == 0)
+                        {
+                            result++;
+                        }
+                    }
+                }
+                n--;
+            }
+
+
+            //return
+            return result;
+
+        }
+
         static void Main(string[] args)
         {
             /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-
+            int x = 100;
+            Console.WriteLine("Test n = {0}", x);
+            Console.WriteLine(Solve(x));
+            Console.ReadKey();
         }
     }
 }
